584c819cd30e316a353ed6158b259aa791be5f0f
Cassie Herd <cassie.herd@gmail.com>
2015-06-11 14:57:19 -0500

    Refactored landing page with jQuery-Checkpoint 29

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,8 +1,27 @@
-var pointsArray = document.getElementsByClassName('point');
+var animatePoints = function() {
+        var revealPoint = function() {
+         // #6
+         $(this).css({
+             opacity: 1,
+             transform: 'scaleX(1) translateY(0)'
+         });
+    };
+    $.each($('.point'), revealPoint);
+};
+$(window).load(function() {
 
-function animatePoints (points, index, pointsArray) {
-        pointsArray[index].style.opacity = 1;
-        pointsArray[index].style.transform = "scaleX(1) translateY(0)";
-        pointsArray[index].style.msTransform = "scaleX(1) translateY(0)";
-        pointsArray[index].style.WebkitTransform = "scaleX(1) translateY(0)"; 
-}
+    // Automatically animates the points on a tall screen where scrolling can't trigger the animation
+    if ($(window).height() > 950) {
+        animatePoints();
+        //Array.prototype.forEach.call(pointsArray, animatePoints);
+    }
+
+    $(window).scroll(function(event) {
+//        if (pointsArray[0].getBoundingClientRect().top <= 500) {
+//            Array.prototype.forEach.call(pointsArray, animatePoints);
+        if ($(window).scrollTop() >= 500) {
+         animatePoints();
+    }
+    });
+
+});
